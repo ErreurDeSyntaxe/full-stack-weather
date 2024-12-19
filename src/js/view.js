27cf7da0b99abd50d14831b81c7b5ce3446d4481
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-12-19 10:23:14 +0800

    Log weather data in console

--- src/js/view.js
+++ src/js/view.js
@@ -1,6 +1,16 @@
 class View {
-  renderMessage(message) {
-    console.log(message);
+  /**
+   * Logs the weather data in the console
+   * @param {string} city
+   * @param {string} country
+   * @param  {...any} arr Three days' worth of weather data
+   */
+  renderMessage(city, country, ...arr) {
+    console.log('=======================');
+    arr.forEach((day) => {
+      let str = `In ${city}, ${country}, the temperature is ${day.currTemp}, but the apparent temperature is ${day.appTemp}. The minimum for the day is ${day.minTemp}, and the maximum is ${day.maxTemp}. There is a ${day.preciPr}% chance of precipitation.`;
+      console.log(str);
+    });
   }
 }
 
